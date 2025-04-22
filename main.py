28301bde188218308a4d22026579fa84d7f4cf44
GaddoStaffu <jomarilagumen02@gmail.com>
2025-04-23 03:20:55 +0800

    added git ignore

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import tcod
 
+from actions import EscapeAction, MovementAction
+from input_handlers import EventHandler
 
 def main() -> None:
     screen_width = 80
@@ -11,21 +13,34 @@ def main() -> None:
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
-
-    with tcod.context.new_terminal(
-        screen_width,
-        screen_height,
+    event_handler = EventHandler()
+    with tcod.context.new(
+        width=screen_width,
+        height=screen_height,
         tileset=tileset,
         title="Yet Another Roguelike Tutorial",
         vsync=True,
     ) as context:
-        root_console = tcod.Console(screen_width, screen_height, order="F")
+        root_console = tcod.console.Console(screen_width, screen_height, order="F")
         while True:
             root_console.print(x=player_x, y=player_y, string="@")
 
             context.present(root_console)
 
             for event in tcod.event.wait():
+                
+                action = event_handler.dispatch(event)
+                if action is None:
+                    continue
+                
+                if isinstance(action, MovementAction):
+                    dx, dy = action.dx, action.dy
+                    player_x += dx
+                    player_y += dy
+                
+                elif isinstance(action, EscapeAction):
+                    raise SystemExit()
+                
                 if event.type == "QUIT":
                     raise SystemExit()
 
